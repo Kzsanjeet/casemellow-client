ed85c2727d06e0e09ba5aaa27f8c38c118c9cb41
Kzsanjeet <sanjeetkazithapa@gmail.com>
2025-05-05 18:43:38 +0545

    arranged responsive

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -486,13 +486,14 @@ const ProductPage = () => {
                 <Button onClick={clearFilters}>Clear All Filters</Button>
               </div>
             ) : (
-              <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto">
-                {productDetails.map((product) => (
-                  <div key={product._id}>
-                    <Card product={product} />
-                  </div>
-                ))}
-              </div>
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 ml-1 mx-auto">
+              {productDetails.map((product) => (
+                <div key={product._id}>
+                  <Card product={product} />
+                </div>
+              ))}
+            </div>
+            
             )}
 
             {/* Pagination Controls */}
--- src/components/search/MainSearch.tsx
+++ src/components/search/MainSearch.tsx
@@ -471,7 +471,7 @@ const MainSearch = () => {
                 <Button onClick={clearFilters}>Clear All Filters</Button>
               </div>
             ) : (
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 ml-1 gap-6">
                 {productDetails.map((product) => (
                   <div key={product._id} className="transition-alls">
                     <Card product={product} />
